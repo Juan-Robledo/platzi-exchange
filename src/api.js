500ca55786f247535605491123ba6528f960753a
Juan-Robledo <robledojmutn@gmail.com>
2021-05-25 15:25:12 -0300

    se agrego archivos al proyecto

---
+++ src/api.js
@@ -0,0 +1,45 @@
+const url = "https://api.coincap.io/v2";
+
+function getAssets() {
+  return fetch(`${url}/assets?limit-20`)
+    .then((res) => res.json())
+    .then((res) => res.data);
+  // .catch(err => console.log(err))
+}
+
+function getAsset(coin) {
+  return fetch(`${url}/assets/${coin}`)
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+
+function getAssetHistory(coin) {
+  // const start = new Date();
+  // const end = start.getTime();
+  // start.setDate(start.getDate() - 1);
+  return fetch(
+    `${url}/assets/${coin}/history?interval=d1`
+  )
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+
+function getMarkets (coin) {
+  return fetch(`${url}/assets/${coin}/markets?limit=5`)
+    .then(res => res.json)
+    .then(res => res.data)
+}
+
+function getExchange (id) {
+  return fetch(`${url}/exchanges/${id}`)
+    .then(res => res.json)
+    .then(res => res.data)
+}
+
+export default {
+  getAssets,
+  getAsset,
+  getAssetHistory,
+  getMarkets,
+  getExchange
+};
